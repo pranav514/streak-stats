bdff5773b50ae7c0c1cc1097a5691d7aeb2cc57a
pranav514 <pranavsalunkhe327@gmail.com>
2025-02-18 17:35:12 +0530

    align text to the left

--- src/routes/streakRoute.ts
+++ src/routes/streakRoute.ts
@@ -1,65 +1,72 @@
-import express, { Router } from "express"
-import { calculateStreak, fetchContributions } from "../utils/fetchContributions";
+import express, { Router } from "express";
+import {
+  calculateStreak,
+  fetchContributions,
+} from "../utils/fetchContributions";
 const router = Router();
-router.get('/streak/:username' , async(req , res) => {
-    try {
-        const username = req.params.username;
-        const weeks = await fetchContributions(username);
-        const streakData = calculateStreak(weeks);
+router.get("/streak/:username", async (req, res) => {
+  try {
+    const username = req.params.username;
+    const weeks = await fetchContributions(username);
+    const streakData = calculateStreak(weeks);
 
-        res.json({
-            username,
-            currentStreak: streakData.currentStreak,
-            maxStreak: streakData.maxStreak,
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Failed to fetch data" });
-    }
-})
+    res.json({
+      username,
+      currentStreak: streakData.currentStreak,
+      maxStreak: streakData.maxStreak,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
 router.get("/streak-svg/:username", async (req, res) => {
     try {
-        const username = req.params.username;
-        const { weeks, totalCommits, totalIssues, totalPRs } = await fetchContributions(username);
-        const { currentStreak, maxStreak } = calculateStreak(weeks);
-        const now = new Date();
-        const daysInMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
-        const badgeEarned = currentStreak === daysInMonth;
-        const svg = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="400" height="220">
-                <rect width="100%" height="100%" fill="white" />
-                <text x="50%" y="15%" fill="blue" font-size="18" font-family="Arial, sans-serif" >
-                    🔥 Current Streak: ${currentStreak} days
-                </text>
-                <text x="50%" y="30%" fill="blue" font-size="18" font-family="Arial, sans-serif" text-anchor="left">
-                    🏆 Max Streak: ${maxStreak} days
-                </text>
-                <text x="50%" y="45%" fill="blue" font-size="18" font-family="Arial, sans-serif" text-anchor="left">
-                    📌 Total Commits: ${totalCommits}
-                </text>
-                <text x="50%" y="60%" fill="blue" font-size="18" font-family="Arial, sans-serif" text-anchor="left">
-                    ❗ Total Issues: ${totalIssues}
-                </text>
-                <text x="50%" y="75%" fill="blue" font-size="18" font-family="Arial, sans-serif" text-anchor="left">
-                    🔄 Total PRs: ${totalPRs}
-                </text>
-                ${
+      const username = req.params.username;
+      const { weeks, totalCommits, totalIssues, totalPRs } =
+        await fetchContributions(username);
+      const { currentStreak, maxStreak } = calculateStreak(weeks);
+      const now = new Date();
+      const daysInMonth = new Date(
+        now.getFullYear(),
+        now.getMonth() + 1,
+        0
+      ).getDate();
+      const badgeEarned = currentStreak === daysInMonth;
+      const svg = `
+              <svg xmlns="http://www.w3.org/2000/svg" width="400" height="220">
+                  <rect width="100%" height="100%" fill="#f8f9fa" />
+                  <text x="30%" y="15%" fill="blue" font-size="18" font-family="Arial, sans-serif" text-anchor="middle" >
+                      🔥 Current Streak: ${currentStreak} days
+                  </text>
+                  <text x="27%" y="30%" fill="blue" font-size="18" font-family="Arial, sans-serif" text-anchor="middle">
+                      🏆 Max Streak: ${maxStreak} days
+                  </text>
+                  <text x="26%" y="45%" fill="blue" font-size="18" font-family="Arial, sans-serif" text-anchor="middle">
+                      📌 Total Commits: ${totalCommits}
+                  </text>
+                  <text x="22%" y="60%" fill="blue" font-size="18" font-family="Arial, sans-serif" text-anchor="middle">
+                      ❗ Total Issues: ${totalIssues}
+                  </text>
+                  <text x="22%" y="75%" fill="blue" font-size="18" font-family="Arial, sans-serif" text-anchor="middle">
+                      🔄 Total PRs: ${totalPRs}
+                  </text>
+                  ${
                     badgeEarned
-                        ? `<text x="50%" y="90%" fill="gold" font-size="20" font-family="Arial, sans-serif" text-anchor="middle">
-                            🏅 ${now.toLocaleString("default", { month: "long" })} Streak Badge
-                           </text>`
-                        : "0"
-                }
-            </svg>
-        `;
-
-        res.setHeader("Content-Type", "image/svg+xml");
-        res.send(svg);
+                      ? `<text x="50%" y="90%" fill="gold" font-size="20" font-family="Arial, sans-serif" text-anchor="middle">
+                              🏅 ${now.toLocaleString("default", {
+                                month: "long",
+                              })} Streak Badge
+                             </text>`
+                      : ""  
+                  }
+              </svg>
+          `;
+  
+      res.setHeader("Content-Type", "image/svg+xml");
+      res.send(svg);
     } catch (error) {
-        res.status(500).send("Failed to generate streak image");
+      res.status(500).send("Failed to generate streak image");
     }
-});
+  });
 
-
-
-
-export default router
+export default router;
